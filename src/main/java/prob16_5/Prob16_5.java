d7e793739dbc028009d02b4e824a364ee99265ff
JuhoKang <rjduakcjs@gmail.com>
2018-01-06 14:43:11 +0900

    add main method with scanner input

--- src/main/java/prob16_5/Prob16_5.java
+++ src/main/java/prob16_5/Prob16_5.java
@@ -1,12 +1,16 @@
 package prob16_5;
 
+import java.util.Scanner;
+
 public class Prob16_5 {
 
   public static void main(String[] args) {
-
+    Scanner sc = new Scanner(System.in);
+    int number = sc.nextInt();
+    System.out.println(getZeroCount(createFactorial(number)));
   }
 
-  public long createFactorial(long num) {
+  public static long createFactorial(long num) {
     long result = 1;
     for(long i = num; i > 1; i--) {
       result *= num;
@@ -15,7 +19,7 @@ public class Prob16_5 {
     return result;
   }
 
-  public int getZeroCount(long num) {
+  public static int getZeroCount(long num) {
     int count = 0;
     while(num % 10 == 0) {
       count++;
