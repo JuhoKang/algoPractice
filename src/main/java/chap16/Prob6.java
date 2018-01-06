cd2c89589011eefcb6b90557f22af6b4197d1f9f
JuhoKang <rjduakcjs@gmail.com>
2018-01-06 17:12:11 +0900

    start of chap16 prob 6

---
+++ src/main/java/chap16/Prob6.java
@@ -0,0 +1,9 @@
+package chap16;
+
+public class Prob6 {
+
+  public static void main(String[] args) {
+    
+  }
+
+}
---
+++ src/test/java/chap16/Prob6Test.java
@@ -0,0 +1,21 @@
+package chap16;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class Prob6Test {
+
+  private Prob6 app;
+
+  @Before
+  public void init() {
+    app = new Prob6();
+  }
+
+  @Test
+  public void 두배열의_최소차_구하기() {
+
+  }
+}
