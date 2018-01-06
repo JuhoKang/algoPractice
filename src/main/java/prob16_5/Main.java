bcfc1bd4ffdb006fc597c7d68c70293f6da566b0
JuhoKang <rjduakcjs@gmail.com>
2018-01-06 14:25:27 +0900

    Intial commit

---
+++ src/main/java/prob16_5/Main.java
@@ -0,0 +1,7 @@
+package prob16_5;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/test/java/prob16_5/Prob16_5Test.java
@@ -0,0 +1,14 @@
+package prob16_5;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class Prob16_5Test {
+
+    @Test
+    public void 앱테스트() {
+        assertEquals(true, true);
+    }
+
+}
